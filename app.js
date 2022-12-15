0946ba6fc8bcf3f56817f54248e037022341613d
rsl150101 <dnwnd501@naver.com>
2022-12-16 00:15:24 +0900

    라우터 설정
    posts, comments 라우터 설정

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const commentRouter = require("./routers/comments");
+const postRouter = require("./routers/posts");
 
 const app = express();
-
 const PORT = 3000;
 
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
 app.listen(PORT, () => {
   console.log(`✅ 서버가 연결되었습니다. http://localhost:${PORT}`);
 });
